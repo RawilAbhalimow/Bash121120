80a0f16952e0b5546b87ed85ad25f9e6aaa73114
Rawil Abkhalimow <117981152+RawilAbhalimow@users.noreply.github.com>
2022-11-13 17:45:06 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-
 if [ $# -eq 0 ]
    then
 	SERVER="Default"
@@ -8,9 +7,19 @@ fi
 
 CURR_DATE=$(date +'%d_%m_%Y')
 FILENAME=./${SERVER}_${CURR_DATE}_running.out
+
+
 curl https://raw.githubusercontent.com/GreatMedivack/files/master/list.out -s | grep Running | awk '{print $1}' > $FILENAME
 
 mkdir ./archives 2> /dev/null
 tar -czf ./archives/${SERVER}_${CURR_DATE}.tar.gz ./$FILENAME
-rm ./$FILENAME
-tar -tvzf ./archives/${SERVER}_${CURR_DATE}.tar.gz >> /dev/null && echo "Archive created and not corrupted"
+tar -tvzf ./archives/${SERVER}_${CURR_DATE}.tar.gz >> /dev/null && GOOD=1
+
+if [ $GOOD -eq 1 ]
+	then
+		echo "Archive created and not corrupted!"
+	else
+		echo "Archive damaged"
+fi
+
+rm -f $FILENAME
